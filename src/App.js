c9de8a8a5fcd28b7a94c2c80a4b2e9fe663afbf3
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-30 12:38:50 +0630

    Events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
-import Clock from "./Clock/Clock";
-function App() {
-    return (
-        <div className="App">
-            <Clock />
-        </div>
-    );
+
+class App extends React.Component {
+    state = {
+        isToggle: false,
+    };
+
+    toggle = () => {
+        this.setState((state) => ({ isToggle: !state.isToggle }));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.isToggle ? "Login" : "Register"}
+                <button onClick={this.toggle}>{this.state.isToggle ? "Yes" : "No"}</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Clock from "./Clock/Clock";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Clock />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
