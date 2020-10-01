3dd5998df5564c1bf1b7bf87db38e36a5e0537c5
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-01 14:31:45 +0630

    Day 1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import "./App.css";
 
 class App extends React.Component {
     render() {
-        const styleBtn = {
-            backgroundColor: "#111",
-            color: "#f1f1f1",
-            padding: "10px",
-            border: "none",
-            borderRadius: "10px",
-        };
         return (
             <div className="App">
-                <button style={styleBtn}>Click</button>
+                <button>Click</button>
             </div>
         );
     }
