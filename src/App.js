7632df490f9f028ade131dce1b11d921002fe33f
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-01 14:35:39 +0630

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-    render() {
-        return <div className="App">Hello, world!</div>;
-    }
+function App() {
+    return <div className="App">Hello, world!</div>;
 }
 export default App;
