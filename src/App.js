1bafa8c342da29934f5e6e3c128534866d654132
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-02 22:54:49 +0630

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Calculator from "./Calculator/Calculator";
+import Clock from "./Clock/Clock";
 class App extends React.Component {
     render() {
-        return <Calculator />;
+        return <Clock />;
     }
 }
 
