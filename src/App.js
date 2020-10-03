3a82c7a7e7de6e7154e3f67930e864ef5b4da1fa
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-03 22:50:03 +0630

    Day 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Clock from "./Clock/Clock";
 class App extends React.Component {
     render() {
-        return <Clock />;
+        return <h1>HELLO OCTOBER</h1>;
     }
 }
 export default App;
