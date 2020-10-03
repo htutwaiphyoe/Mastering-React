0148223661be65476380765440d6614326c82718
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-03 23:55:44 +0630

    Day 3

--- src/App.js
+++ src/App.js
@@ -10,10 +10,19 @@ class App extends React.Component {
         this.setState({ toggle: !this.state.toggle });
     };
     render() {
+        const styleBtn = {
+            backgroundColor: "#111",
+            color: "#f1f1f1",
+            padding: "10px",
+            border: "none",
+            borderRadius: "10px",
+        };
         return (
             <div className="App">
                 <p>{this.state.toggle ? "Yes" : "No"}</p>
-                <button onClick={this.toggle}>Click</button>
+                <button onClick={this.toggle} style={styleBtn}>
+                    Click
+                </button>
             </div>
         );
     }
