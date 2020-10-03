9ed5450bbc1d04b77de629e47195fa8b5ca75819
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-03 23:43:55 +0630

    Day 3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./App.css";
 
 const Person = (props) => {
-    return <p>Hello, {props.name}!</p>;
+    return <p>Hello, {props.children}!</p>;
 };
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Person name="Htut Wai Phyoe" />
-                <Person name="Timmy" />
-                <Person name="Timx" />
+                <Person>Htut Wai Phyoe</Person>
             </div>
         );
     }
