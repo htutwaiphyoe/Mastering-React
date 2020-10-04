40cb938d7139491b33eca6a0b54099cdacdd87bc
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-04 16:04:05 +0630

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
-
-function App() {
-    return <div className="App">Hello, world!</div>;
+import Person from "./Person/Person";
+class App extends React.Component {
+    state = {
+        persons: [
+            { name: "Jane", age: 20 },
+            { name: "George", age: 21 },
+            { name: "Emily", age: 22 },
+        ],
+    };
+    render() {
+        return (
+            <div className="App">
+                <button>Switch</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+    }
 }
 
 export default App;
