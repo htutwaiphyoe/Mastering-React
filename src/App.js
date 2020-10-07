46f00604935ac5408278b3031f91e2625cdd41d9
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-07 11:50:57 +0630

    List and Key

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Person from "./Person/Person";
 class App extends React.Component {
     state = {
         persons: [
-            { name: "Jane", age: 20 },
-            { name: "George", age: 21 },
-            { name: "Emily", age: 22 },
+            { id: "1212", name: "Jane", age: 20 },
+            { id: "asdf", name: "George", age: 21 },
+            { id: "121212", name: "Emily", age: 22 },
         ],
         show: false,
     };
@@ -16,11 +16,35 @@ class App extends React.Component {
             show: !this.state.show,
         });
     };
+
+    deletePersonHandler = (index) => {
+        const persons = [...this.state.persons];
+
+        persons.splice(index, 1);
+        console.log(persons);
+        this.setState({ persons });
+    };
+
+    inputHandler = (e, id) => {
+        const personIndex = this.state.persons.findIndex((p) => p.id === id);
+        const person = { ...this.state.persons[personIndex] };
+        person.name = e.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+        this.setState({ persons });
+    };
     render() {
         let element = null;
         if (this.state.show) {
-            element = this.state.persons.map((person) => (
-                <Person name={person.name} age={person.age} />
+            element = this.state.persons.map((person, index) => (
+                <Person
+                    name={person.name}
+                    key={person.id}
+                    age={person.age}
+                    click={this.deletePersonHandler.bind(this, index)}
+                    change={(e) => this.inputHandler(e, person.id)}
+                    val={person.name}
+                />
             ));
         }
         return (
