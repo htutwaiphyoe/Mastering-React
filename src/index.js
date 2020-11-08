0d0be46d1e2000d8e46dc5a9e6924d33c36e6f2d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-11-08 06:33:50 +0630

    Custom Middleware & Redux DevTool

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./redux/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./redux/store/reducers";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
@@ -33,7 +33,20 @@ const responseInterceptor = axios.interceptors.response.use(
     }
 );
 axios.interceptors.response.eject(responseInterceptor);
-const store = createStore(reducer);
+// Customer Middleware
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+            console.log("Dispatch", action);
+            const result = next(action);
+            console.log("New state", store.getState(), result);
+            return result;
+        };
+    };
+};
+// Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
