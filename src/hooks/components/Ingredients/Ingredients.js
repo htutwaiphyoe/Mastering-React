11f569ef56e1f679c9ddf1ffcb33805c5ee87176
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-11-21 18:10:32 +0630

    useEffect

--- src/hooks/components/Ingredients/Ingredients.js
+++ src/hooks/components/Ingredients/Ingredients.js
@@ -6,6 +6,17 @@ import Search from "./Search";
 
 const Ingredients = (props) => {
     const [ingredients, setIngredients] = useState([]);
+    useEffect(() => {
+        fetch("https://react-hooks-355b5.firebaseio.com/ingredients.json")
+            .then((response) => response.json())
+            .then((res) => {
+                const data = [];
+                for (let i in res) {
+                    data.push(res[i]);
+                }
+                setIngredients((prevIngredients) => [...prevIngredients, ...data]);
+            });
+    }, []);
     const addIngredients = async (data) => {
         const response = await fetch("https://react-hooks-355b5.firebaseio.com/ingredients.json", {
             method: "POST",
